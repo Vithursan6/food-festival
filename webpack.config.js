b7a1cb9c5eb636f39236aa4283f0bd488347b106
Vithursan6 <93847590+Vithursan6@users.noreply.github.com>
2022-03-24 18:38:35 -0400

    added file-loader and image-loader

--- webpack.config.js
+++ webpack.config.js
@@ -12,8 +12,32 @@ module.exports = {
     },
     output: {
             filename: "[name].bundle.js",
-            path: __dirname + 'dist',
+            path: __dirname + '/dist',
             },
+    module: {
+        rules: [
+            {
+                test: /\.jpg$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            esModule: false,
+                            name (file) {
+                                return "[path][name].[ext]"
+                            },
+                            publicPath: function(url) {
+                                return url.replace("../", "/assets/")
+                            }
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader'
+                    }
+                ]
+            }
+        ]
+    },
     plugins: [
         new webpack.ProvidePlugin({
             $: "jquery",
